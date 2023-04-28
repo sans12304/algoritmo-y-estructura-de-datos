95c37aa63c5f52fcf93570a1343eae61c066afe4
sans12304 <60119987+sans12304@users.noreply.github.com>
2023-04-28 13:16:18 +0000

    Actividad 7

--- TP 1/Actividad 7.C
+++ TP 1/Actividad 7.C
@@ -6,9 +6,9 @@ int monto_en_pesos;
 main(){
     printf("Ingrese el monto en pesos: ");
     scanf("%d", &monto_en_pesos);
-    printf("El %80 del monto ingresado es: %d \n", monto_en_pesos*(0.80));
-    printf("El %20 del monto ingresado es: %d \n", monto_en_pesos*(0.20));
-    printf("El %15 del monto ingresado es: %d \n", monto_en_pesos*(0.15));
+    printf("El 80 porciento del monto ingresado es: %f \n", monto_en_pesos*0.8);
+    printf("El 20 porciento del monto ingresado es: %f \n", monto_en_pesos*0.2);
+    printf("El 15 porciento del monto ingresado es: %f \n", monto_en_pesos*0.15);
     system("pause");
     return(0);
 }
