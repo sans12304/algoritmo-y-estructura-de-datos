ec61f9bb29827150be5963bd8eaf12c5d11765bf
sans12304 <60119987+sans12304@users.noreply.github.com>
2023-04-28 02:27:07 +0000

    test

---
+++ TP 1/Actividad 6.C
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int numero_uno
+int numero_dos
+int numero_tres
+
+main(){
+    //Ingreso de datos
+    printf("Ingrese el primer numero: ");
+    scanf("%i", &numero_uno);
+    printf("Ingrese el segundo numero: ");
+    scanf("%i", &numero_dos);
+    printf("Ingrese el tercer numero: ");
+    scanf("%i", &numero_tres);
+    
+}
