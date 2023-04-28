22f4ff559d7ebdfc336f687b84e8e20d9eed024a
sans12304 <60119987+sans12304@users.noreply.github.com>
2023-04-28 13:25:09 +0000

    Actividad 8

---
+++ TP 1/Actividad 8.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int monto_en_pesos;
+int precio_dolar = 15.40;
+
+int main(int argc, char const *argv[])
+{
+    printf("Ingrese el monto en pesos: \n");
+    scanf("%i", &monto_en_pesos);
+    printf("El monto ingresado equivale a %d dolares", monto_en_pesos/precio_dolar);
+    return 0;
+}
