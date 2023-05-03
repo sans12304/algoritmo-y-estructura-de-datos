3265631cff298a419c83d0c2d5ed47751eea19a0
sans12304 <60119987+sans12304@users.noreply.github.com>
2023-05-03 02:57:19 +0000

    terminar 10

--- TP 1/Actividad 10.C
+++ TP 1/Actividad 10.C
@@ -9,6 +9,5 @@ main(){
     scanf("%f", &cateto1);
     printf("Introdusca el segundo cateto: \n");
     scanf("%f", &cateto2);
-    printf("La hipotenusa del triangulo es: %f\n", sqrt(cateto1 * cateto1 + cateto2 * cateto2));
-    return 0;
+    printf("La hipotenusa del triangulo es: %f\n",);
 }
