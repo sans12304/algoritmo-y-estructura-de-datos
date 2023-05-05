0e069e4657aeb0a0b27b9f9d2eddc2988e2bb2f4
sans12304 <60119987+sans12304@users.noreply.github.com>
2023-05-05 12:02:46 +0000

    Actividad 10

--- TP 1/Actividad 10.C
+++ TP 1/Actividad 10.C
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 float cateto1;
 float cateto2;
@@ -9,5 +10,8 @@ main(){
     scanf("%f", &cateto1);
     printf("Introdusca el segundo cateto: \n");
     scanf("%f", &cateto2);
-    printf("La hipotenusa del triangulo es: %f\n",);
+    printf("La hipotenusa del triangulo es: %f\n" , sqrt(pow(cateto1,2)+pow(cateto2,2)));
+
+    system("pause");
+    return(0);
 }
