562b71e7550142f5005f6a550b760c939b490045
sans12304 <60119987+sans12304@users.noreply.github.com>
2023-05-22 00:05:57 +0000

    anache
    Co-authored-by: Juan <juankionbush@users.noreply.github.com>

---
+++ Parcial/Segundo Parcial.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int canal, televidentes, rating, televidentes_prime, puntos_rating_9, puntos_rating_11, puntos_rating_13;
+    char horario;
+
+}
