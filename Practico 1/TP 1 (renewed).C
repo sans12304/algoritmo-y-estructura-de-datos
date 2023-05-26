e783d74236613bf7f89de3e03047c1498fa7fa6c
Tomás <prietostomas@outlook.com>
2023-05-26 20:13:11 -0300

    anashe

--- Practico 1/TP 1 (renewed).C
+++ Practico 1/TP 1 (renewed).C
@@ -1,54 +1,54 @@
-
 #include <stdio.h>
 #include <string.h>
-// Función para calcular el precio total de una lista de productos
-float calcularPrecioTotal(float precios[], int cantidad) {
+
+float calculargastos(float precios[], int cantidad) {
     float total = 0;
-    for (int i = 0; i < cantidad; i++) {
+    for (int i = 0; i < cantidad;) {
         total += precios[i];
+        i++;
     }
     return total;
 }
-// Función para mostrar información detallada de un producto
-void mostrarInfoProducto(char nombre[], float precio) {
+void MostrarGastos(char nombre[], float precio) {
     printf("Producto: %s\n", nombre);
     printf("Precio: %.2f\n", precio);
 }
+
 int main(){
-    int opcion;
+    int opcion = 0;
     int cantidad_productos = 0;
-    char nombre_producto[50];
+    char nombre_gasto[50];
     float precios[50];
-    while (1) {
-        printf("=-=-=-=-=-=-= Supermercado =-=-=-=-=-=-=\n");
-        printf("1. Agregar producto\n");
-        printf("2. Mostrar información de todos los productos\n");
-        printf("3. Calcular precio total\n");
+    while (opcion != 4) {
+        printf("- - - - - Control de Gastos - - - - -\n");
+        printf("1. Agregar Gasto\n");
+        printf("2. Mostrar Todos los gastos\n");
+        printf("3. Calcular total\n");
         printf("4. Salir\n");
         printf("Ingrese una opción: ");
         scanf("%d", &opcion);
         switch (opcion) {
             case 1:
                 printf("Ingrese el nombre del producto: ");
-                scanf(" %[^\n]s", nombre_producto);
+                scanf(" %[^\n]s", nombre_gasto);
                 printf("Ingrese el precio del producto: ");
                 scanf("%f", &precios[cantidad_productos]);
                 cantidad_productos++;
                 printf("Producto agregado con éxito.\n\n");
                 break;
             case 2:
-                printf("=-=-=-=-=-=-= Información de los productos =-=-=-=-=-=-=\n");
+                printf("- - - - - Información de los gastos - - - - -\n");
                 for (int i = 0; i < cantidad_productos;){
-                    mostrarInfoProducto(nombre_producto, precios[i]);
+                    MostrarGastos(nombre_gasto, precios[i]);
                     i++;
                 }
                 printf("\n");
                 break;
             case 3:
-                printf("El precio total de los productos es: %.2f\n\n", calcularPrecioTotal(precios, cantidad_productos));
+                printf("El total de los gastos es: %.2f\n\n", calculargastos(precios, cantidad_productos));
                 break;
             case 4:
-                printf("Gracias por utilizar nuestra tienda. ¡Hasta luego!\n");
+                printf("Gracias por utilizar nuestra servicio.\n");
                 return 0;
             default:
                 printf("Opción inválida. Por favor, ingrese un número válido.\n\n");
