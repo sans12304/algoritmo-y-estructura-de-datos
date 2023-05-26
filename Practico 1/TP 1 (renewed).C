ad599a254151644881466e3c37cb9fadc95a88ff
Tomás <prietostomas@outlook.com>
2023-05-26 20:16:14 -0300

    .

--- Practico 1/TP 1 (renewed).C
+++ Practico 1/TP 1 (renewed).C
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-<<<<<<< HEAD:Practico 1/TP 1 (renewed).C
 
 float calculargastos(float precios[], int cantidad) {
-=======
-float calcularPrecioTotal(float precios[], int cantidad) {
->>>>>>> 3d7cfffa74b28075338d21a564c568b6e951a216:Practico 1/TP N°1.C
     float total = 0;
     for (int i = 0; i < cantidad;) {
         total += precios[i];
@@ -13,11 +9,7 @@ float calcularPrecioTotal(float precios[], int cantidad) {
     }
     return total;
 }
-<<<<<<< HEAD:Practico 1/TP 1 (renewed).C
 void MostrarGastos(char nombre[], float precio) {
-=======
-void mostrarInfoProducto(char nombre[], float precio) {
->>>>>>> 3d7cfffa74b28075338d21a564c568b6e951a216:Practico 1/TP N°1.C
     printf("Producto: %s\n", nombre);
     printf("Precio: %.2f\n", precio);
 }
